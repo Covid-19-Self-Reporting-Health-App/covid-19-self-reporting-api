1b377959869792270585a3bcac3c74b4fae971ee
Ian Roberts <irob466@gmail.com>
2020-03-28 22:06:36 -0500

    can now send and receive data back from redis

--- src/server.js
+++ src/server.js
@@ -1,9 +1,29 @@
 // server.js
 const express = require("express");
 const app = express();
+const redisClient = require("./redis-client");
 
 app.get("/", (req, res) => {
   return res.send("Hello bitch!");
+});
+
+app.get("/store/:key", async ({ params: { key }, query }, res, next) => {
+  try {
+    await redisClient.setAsync(key, JSON.stringify(query));
+    return res.send("success");
+    // return res.status(200).json(JSON.parse(success));
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+app.get("/:key", async ({ params: { key } }, res, next) => {
+  try {
+    const success = await redisClient.getAsync(key);
+    return res.status(200).json(JSON.parse(success));
+  } catch (e) {
+    console.error(e);
+  }
 });
 
 const PORT = process.env.PORT || 3000;
