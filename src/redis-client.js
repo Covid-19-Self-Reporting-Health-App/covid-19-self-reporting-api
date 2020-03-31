46d099b916c08ff34e4bc96a8731fb635957c208
Ian Roberts <irob466@gmail.com>
2020-03-30 22:19:17 -0500

    configured redis url for local dev

--- src/redis-client.js
+++ src/redis-client.js
@@ -1,11 +1,13 @@
 // redis-client.js
 const redis = require('redis');
 const {promisify} = require('util');
-const client = redis.createClient(process.env.REDIS_URL);
+const client = redis.createClient(
+  process.env.REDIS_URL || 'redis://127.0.0.1:6379'
+);
 
 module.exports = {
   ...client,
   getAsync: promisify(client.get).bind(client),
   setAsync: promisify(client.set).bind(client),
-  keysAsync: promisify(client.keys).bind(client),
+  keysAsync: promisify(client.keys).bind(client)
 };
